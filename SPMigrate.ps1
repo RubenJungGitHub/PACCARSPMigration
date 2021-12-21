6dc46b2117e18ac834217af67a55e128bd6375b9
RubenJungGitHub <71824094+RubenJungGitHub@users.noreply.github.com>
2021-12-21 12:53:18 +0100

    Try fix connection issue SG

--- SPMigrate.ps1
+++ SPMigrate.ps1
@@ -3,7 +3,8 @@ param()
 
 #initialize
 #Start-MtHLocalPowerShell -settingfile "$(Get-MtHGitDirectory)\settings.json" -initsp -Verbose
-Start-MtHLocalPowerShell -settingfile "$(Get-MtHGitDirectory)\settings.json" -Verbose -initsp
+#start-MtHLocalPowerShell -settingfile "$(Get-MtHGitDirectory)\settings.json" -Verbose -initsp
+start-MtHLocalPowerShell -settingfile "$(Get-MtHGitDirectory)\settings.json" -Verbose 
 # open the demo site and fill the Demo Library
 
 # Create testdata : check if the SP demo library (locally stored) is already filled, if not fill it
