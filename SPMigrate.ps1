e5ebc682b3327903bf91c8f5939bb53672b96350
RubenJungGitHub <71824094+RubenJungGitHub@users.noreply.github.com>
2022-01-21 14:07:03 +0100

    Added deactivation l function

--- SPMigrate.ps1
+++ SPMigrate.ps1
@@ -18,7 +18,7 @@ Write-Verbose "Used Database = $($settings.SQLDetails.Name),$($settings.SQLDetai
 #$ModulePath = -Join ($Settings.FilePath.LocalWorkSpaceModule, 'Public')
 #Set-Location -Path $ModulePath
 do {
-    $action = ('++++++++++++++++++++++++++++++++++++++++++++++++++++++', 'Create DataBase', 'Remove DataBase', 'Deactivate Set of Sites and Lists for migration', 'Register Set of Sites and Lists for first migration', 'Register Set of Sites and Lists for delta migration', 'Test SP connections', 'Reset runs after truncation', 'Migrate Real', 
+    $action = ('++++++++++++++++++++++++++++++++++++++++++++++++++++++', 'Create DataBase', 'Remove DataBase', 'Deactivate Set of Sites and Lists in DB', 'Register Set of Sites and Lists for first migration', 'Register Set of Sites and Lists for delta migration', 'Test SP connections', 'Reset runs after truncation', 'Migrate Real', 
         'Delete MU-s from target', 'Create MenuItem', 'Quit') | Out-GridView -Title 'Choose Activity (Only working on dev and test env)' -PassThru
     #Make sure the testprocedures only access Dev and test.
     switch ($action) {
@@ -36,10 +36,15 @@ do {
             Start-RJDBRegistrationCycle
             Register-MtHAllSitesLists 
         }
-        'Deactivate Set of Sites and Lists for migration' {
+        'Deactivate Set of Sites and Lists in DB' {
             #not included
-            $Items = Start-RJDBRegistrationCycle -NextAction "First"
-            If ($Null -ne $Items) { Register-MtHAllSitesLists -MUsINSP $Items -NextAction "none" }
+            $Items = Start-RJDBRegistrationCycle -NextAction "None"
+            ForEach ($Item in $Items) {
+                $sql = @"
+                Update MigrationUnits SET NextAction ='none' where CompleteSourceURL = '$($Item.CompleteSourceURL)' 
+"@
+               Invoke-Sqlcmd -ServerInstance $Settings.SQLDetails.Instance -Database $Settings.SQLDetails.Database -Query $sql
+            }
         }
         'Register Set of Sites and Lists for first migration' {
             #not included
@@ -59,44 +64,44 @@ do {
             $sql = @'
             Delete from MigrationRuns  Where Result = 'Started';
 '@
-        Invoke-Sqlcmd -ServerInstance $Settings.SQLDetails.Instance -Database $Settings.SQLDetails.Database -Query $sql | Resolve-MtHMRClass
-        }
-        'Migrate Fake' {
-            Start-MtHExecutionCycle -Fake    
-        }
-        'Migrate Real' {
-            #Connect-MtHSharePoint
-            Start-MtHExecutionCycle 
-        }
-        'Delete MU-s from target' {
-            $MUsForDeletion = Select-RJMusForDeletion
-            If ($MUsForDeletion) { Start-RJDeletionCycle $MUsForDeletion }
-            Write-Host "Deletion cycle completed" -ForegroundColor Cyan
-        }
-        'Deactive All Test Lists' {
-            $TestSourceURLS = [System.Collections.Generic.List[PSCustomObject]]::new()
-            foreach ($MigUnitURL in $Settings.Current.MigrationURLS) {
-                foreach ($DemoSite  in $MigUnitURL.DemoSite) {
-                    $TestURL = ConvertTo-MtHHttpAbsPath -SourceURL $MigUnitURL.SourceTenantDomain -path $DemoSite
-                    $TestSourceURLS.Add($TestURL)
+                Invoke-Sqlcmd -ServerInstance $Settings.SQLDetails.Instance -Database $Settings.SQLDetails.Database -Query $sql
+            }
+            'Migrate Fake' {
+                Start-MtHExecutionCycle -Fake    
+            }
+            'Migrate Real' {
+                #Connect-MtHSharePoint
+                Start-MtHExecutionCycle 
+            }
+            'Delete MU-s from target' {
+                $MUsForDeletion = Select-RJMusForDeletion
+                If ($MUsForDeletion) { Start-RJDeletionCycle $MUsForDeletion }
+                Write-Host "Deletion cycle completed" -ForegroundColor Cyan
+            }
+            'Deactive All Test Lists' {
+                $TestSourceURLS = [System.Collections.Generic.List[PSCustomObject]]::new()
+                foreach ($MigUnitURL in $Settings.Current.MigrationURLS) {
+                    foreach ($DemoSite  in $MigUnitURL.DemoSite) {
+                        $TestURL = ConvertTo-MtHHttpAbsPath -SourceURL $MigUnitURL.SourceTenantDomain -path $DemoSite
+                        $TestSourceURLS.Add($TestURL)
+                    }
+                }
+                $dbitems = $TestSourceURLS | ForEach-Object { Get-MtHSQLMigUnits -Url $_ }
+                $items = $dbitems | Where-Object { ($_.NextAction -ne 'none') }
+                $items | ForEach-Object {
+                    $Item = [PSCustomObject]@{
+                        MUStatus   = $_.MUStatus 
+                        NextAction = 'none' 
+                        NodeId     = $_.NodeId
+                        MigUnitId  = $_.MigUnitId
+                    }
+                    Update-MtHSQLMigUnitStatus -Item $Item
                 }
             }
-            $dbitems = $TestSourceURLS | ForEach-Object { Get-MtHSQLMigUnits -Url $_ }
-            $items = $dbitems | Where-Object { ($_.NextAction -ne 'none') }
-            $items | ForEach-Object {
-                $Item = [PSCustomObject]@{
-                    MUStatus   = $_.MUStatus 
-                    NextAction = 'none' 
-                    NodeId     = $_.NodeId
-                    MigUnitId  = $_.MigUnitId
-                }
-                Update-MtHSQLMigUnitStatus -Item $Item
+            'Create menuItem' {
+                $URL = "https://paccar.sharepoint.com/sites/DAF-MS-ASCOM-Site"
             }
         }
-        'Create menuItem' {
-            $URL = "https://paccar.sharepoint.com/sites/DAF-MS-ASCOM-Site"
-        }
-    }
-} while (($action -ne 'Quit') -and ($null -ne $action))
+    } while (($action -ne 'Quit') -and ($null -ne $action))
 
-Stop-MtHLocalPowershell
+    Stop-MtHLocalPowershell
